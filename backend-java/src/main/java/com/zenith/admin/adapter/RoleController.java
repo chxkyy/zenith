4b1497e25b38a7462e6309bb45d6ec18ba85b11e
chenxiangkai <chenxiangkai@xyzq.com.cn>
2026-03-25 21:52:15 +0800

    初始提交

--- backend-java/src/main/java/com/zenith/admin/adapter/RoleController.java
+++ backend-java/src/main/java/com/zenith/admin/adapter/RoleController.java
@@ -4,9 +4,7 @@ import com.alibaba.cola.dto.MultiResponse;
 import com.zenith.admin.app.RoleService;
 import com.zenith.admin.dto.RoleDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/roles")
