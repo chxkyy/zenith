a075cf3d003e42d1d799212517f816f785627f79
chenxiangkai <chenxiangkai@xyzq.com.cn>
2026-03-25 21:56:48 +0800

    初始提交

--- backend-java/src/main/java/com/zenith/admin/adapter/NoticeController.java
+++ backend-java/src/main/java/com/zenith/admin/adapter/NoticeController.java
@@ -4,9 +4,7 @@ import com.alibaba.cola.dto.MultiResponse;
 import com.zenith.admin.app.NoticeService;
 import com.zenith.admin.dto.NoticeDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/notices")
