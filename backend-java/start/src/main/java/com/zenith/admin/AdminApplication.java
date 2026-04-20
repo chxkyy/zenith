413e7fed6ff45446001cf369a532eed7c889f4b6
chenxiangkai <chenxiangkai@xyzq.com.cn>
2026-04-20 20:25:05 +0800

    初始提交

--- backend-java/start/src/main/java/com/zenith/admin/AdminApplication.java
+++ backend-java/start/src/main/java/com/zenith/admin/AdminApplication.java
@@ -12,16 +12,5 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class AdminApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(AdminApplication.class, args);
-        
-        // 检查 RoleNameAspect Bean 是否存在
-        if (context.containsBean("roleNameAspect")) {
-            log.info("========== RoleNameAspect Bean 存在 ==========");
-        } else {
-            log.error("========== RoleNameAspect Bean 不存在 ==========");
-        }
-        
-        // 打印所有 Bean 名称
-        String[] beanNames = context.getBeanDefinitionNames();
-        log.info("========== 容器中所有 Bean 数量: {} ==========", beanNames.length);
     }
 }
