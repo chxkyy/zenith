34b0d7537805c7cbe6e2671719a0c15e32be9f47
chenxiangkai <chenxiangkai@xyzq.com.cn>
2026-05-04 14:10:45 +0800

    初始提交

--- backend-java/project-api/src/main/java/com/zenith/admin/api/UserService.java
+++ backend-java/project-api/src/main/java/com/zenith/admin/api/UserService.java
@@ -5,11 +5,17 @@ import com.zenith.admin.dto.data.UserDTO;
 import com.zenith.admin.dto.data.UserPageQuery;
 
 public interface UserService {
-    PageInfo<UserDTO> listByPage(UserPageQuery query);
-    void save(UserDTO userDTO);
-    void update(UserDTO userDTO);
-    void delete(Long id);
-    UserDTO getById(Long id);
-    void resetPassword(Long id);
     void changeStatus(Long id, Integer status);
+
+    void delete(Long id);
+
+    UserDTO getById(Long id);
+
+    PageInfo<UserDTO> listByPage(UserPageQuery query);
+
+    void resetPassword(Long id);
+
+    void save(UserDTO userDTO);
+
+    void update(UserDTO userDTO);
 }
