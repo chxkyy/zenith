ba9b7e3769b8f257b02bab0704c20ab42616f660
chenxiangkai <chenxiangkai@xyzq.com.cn>
2026-05-05 23:15:14 +0800

    初始提交

--- backend-java/project-web/src/main/java/com/zenith/admin/config/SpringSessionConfig.java
+++ backend-java/project-web/src/main/java/com/zenith/admin/config/SpringSessionConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.session.SessionIdGenerator;
+import org.springframework.session.web.http.CookieHttpSessionIdResolver;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
 import org.springframework.session.web.http.SessionRepositoryFilter;
 import org.springframework.transaction.support.TransactionTemplate;
 
@@ -25,7 +28,26 @@ public class SpringSessionConfig {
     }
 
     @Bean
-    public SessionRepositoryFilter<?> sessionRepositoryFilter(CustomJdbcSessionRepository sessionRepository) {
-        return new SessionRepositoryFilter<>(sessionRepository);
+    public CookieSerializer cookieSerializer() {
+        DefaultCookieSerializer serializer = new DefaultCookieSerializer();
+        serializer.setCookieName("ZENITH_TOKEN");
+        serializer.setUseHttpOnlyCookie(true);
+        serializer.setSameSite("Lax");
+        serializer.setCookieMaxAge(1800);
+        serializer.setUseSecureCookie(false);
+//        serializer.setUseSecureFlag(false);
+        serializer.setDomainNamePattern(".*");
+//        serializer.setPath("/");
+        serializer.setCookiePath("/");
+        return serializer;
+    }
+
+    @Bean
+    public SessionRepositoryFilter<?> sessionRepositoryFilter(CustomJdbcSessionRepository sessionRepository, CookieSerializer cookieSerializer) {
+        SessionRepositoryFilter<CustomJdbcSessionRepository.CustomSession> filter = new SessionRepositoryFilter<>(sessionRepository);
+        CookieHttpSessionIdResolver sessionIdResolver = new CookieHttpSessionIdResolver();
+        sessionIdResolver.setCookieSerializer(cookieSerializer);
+        filter.setHttpSessionIdResolver(sessionIdResolver);
+        return filter;
     }
 }
